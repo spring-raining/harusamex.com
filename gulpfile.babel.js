9f5313439a4e56527f039b28c14de00da0b25a2c
spring-raining <fdonthavei@gmail.com>
2016-05-09 02:49:11 +0900

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import gulp from 'gulp';
+import del from 'del';
+import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('default', ['clean'], (callback) =>
+  runSequence(
+    callback
+  )
+);
+
+gulp.task('clean', () => del([], {dot: true}));
+
+gulp.task('html', () =>
+  gulp.src('app/**/*.jade')
+    .pipe($.jade())
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('scripts', () =>
+  gulp.src('app/scripts/**/*.js')
+    .pipe($.browserify({
+      transform: ['babelify'],
+    }))
+    .pipe($.uglify({
+      preserveComment: 'some',
+    }))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/scripts'))
+);
+
+gulp.task('styles', () =>
+  gulp.src([
+    'app/styles/**/*.styl',
+    '!app/styles/**/_*.styl',
+  ])
+    .pipe($.stylus())
+    .pipe(gulp.dest('dist/styles'))
+);
+
